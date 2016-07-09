1bb1a15aa3dc3aab27c56c9d241a64ccdcc71226
kbharathala <kbharathala@gmail.com>
2016-07-09 14:41:56 -0700

    Adding some comments

--- greylock/greylock/ViewController.m
+++ greylock/greylock/ViewController.m
@@ -16,6 +16,15 @@
 
 @implementation ViewController
 
+
+- (id) init {
+    self = [super init];
+    if (self) {
+        self.title = @"Konnected";
+    }
+    return self;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     
@@ -41,6 +50,39 @@
     
 }
 
+/*
+ * Called on App Startup
+ * Sets the phone_id in user_defaults
+ */
+- (void) requestPhoneID {
+    NSString *url = [NSString stringWithFormat:@"http://54.174.96.55:3000/device/initialize"];
+
+    NSURLSession *session = [NSURLSession sharedSession];
+    [[session dataTaskWithURL:[NSURL URLWithString:url]
+            completionHandler:^(NSData *data,
+                                NSURLResponse *response,
+                                NSError *error) {
+                NSHTTPURLResponse *urlResponse = (NSHTTPURLResponse *)response;
+                if (urlResponse.statusCode != 200) {
+                    NSLog(@"Error getting %@, HTTP status code %li", url, (long)urlResponse.statusCode);
+                    return;
+                }
+                
+                NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data
+                                                                     options:kNilOptions
+                                                                       error:&error];
+                
+                NSLog(@"%@", json);
+                NSString *phone_id = [json objectForKey:@"phone_id"];
+                [[NSUserDefaults standardUserDefaults] setObject:phone_id forKey:@"phoneID"];
+                
+            }] resume];
+}
+
+/*
+ * Called when top right join button is pressed
+ * Asks for room number and calls joinRoom;
+ */
 - (void) joinButtonPressed {
     UIAlertController *alertController =
     [UIAlertController alertControllerWithTitle:@"What room do you want to join"
@@ -70,31 +112,50 @@
     [self presentViewController:alertController animated:YES completion:nil];
 }
 
-- (void) requestPhoneID {
-    NSString *url = [NSString stringWithFormat:@"http://54.174.96.55:3000/device/initialize"];
-
+/*
+ * Called by joinButtonPressed after room is entered
+ * Checks if sessionID exists and then opens intermediate VC.
+ */
+- (void) joinRoom:(NSString *)sessionID {
+    NSString *post = [NSString stringWithFormat:@"phone_id=%@&session_id=%@",
+                      [[NSUserDefaults standardUserDefaults] objectForKey:@"phoneID"],
+                      sessionID];
+    NSData *postData = [post dataUsingEncoding:NSASCIIStringEncoding allowLossyConversion:YES];
+    NSString *postLength = [NSString stringWithFormat:@"%lu", (unsigned long)[postData length]];
+    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
+    
+    [request setURL:[NSURL URLWithString: @"http://54.174.96.55:3000/device/register_session"]];
+    [request setHTTPMethod:@"POST"];
+    [request setValue:postLength forHTTPHeaderField:@"Content-Length"];
+    [request setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Content-Type"];
+    [request setHTTPBody:postData];
+    
     NSURLSession *session = [NSURLSession sharedSession];
-    [[session dataTaskWithURL:[NSURL URLWithString:url]
-            completionHandler:^(NSData *data,
-                                NSURLResponse *response,
-                                NSError *error) {
-                NSHTTPURLResponse *urlResponse = (NSHTTPURLResponse *)response;
-                if (urlResponse.statusCode != 200) {
-                    NSLog(@"Error getting %@, HTTP status code %li", url, (long)urlResponse.statusCode);
-                    return;
-                }
-                
-                NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data
-                                                                     options:kNilOptions
-                                                                       error:&error];
-                
-                NSLog(@"%@", json);
-                NSString *phone_id = [json objectForKey:@"phone_id"];
-                [[NSUserDefaults standardUserDefaults] setObject:phone_id forKey:@"phoneID"];
-                
-            }] resume];
+    [[session dataTaskWithRequest:request
+                completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+                    if (data) {
+                        NSDictionary *dict =
+                        [NSJSONSerialization JSONObjectWithData:data
+                                                        options:kNilOptions
+                                                          error:&error];
+                        NSLog(@"%@", dict);
+                        int count = [[dict objectForKey:@"count"] intValue];
+                        
+                        [[NSUserDefaults standardUserDefaults] setObject:sessionID forKey:@"sessionID"];
+                        IntermediateViewController *intermediateVC =
+                        [[IntermediateViewController alloc] initWithCount:count];
+                        [self presentViewController:intermediateVC animated:YES completion:nil];
+                        
+                        
+                    } else {
+                        NSLog(@"ERROR");
+                    }
+                }] resume];
 }
 
+/*
+ * Call when launching widget1, will be refactored into onCollectionViewClick
+ */
 - (void)launchWidget1 {
     UIAlertController *alertController =
     [UIAlertController alertControllerWithTitle:@"How many phones do you want to use?"
@@ -119,12 +180,26 @@
     [self presentViewController:alertController animated:YES completion:nil];
 }
 
+/*
+ * Called by launch widget to go to intermediateViewController.
+ */
 - (void) setWithCount:(int)count {
-    NSString *url = [NSString stringWithFormat:@"http://54.174.96.55:3000/device/create_session/%@",
-                     [[NSUserDefaults standardUserDefaults] objectForKey:@"phoneID"]];
+    NSString *post = [NSString stringWithFormat:@"phone_id=%@",
+                      [[NSUserDefaults standardUserDefaults] objectForKey:@"phoneID"]];
+    NSData *postData = [post dataUsingEncoding:NSASCIIStringEncoding allowLossyConversion:YES];
+    NSString *postLength = [NSString stringWithFormat:@"%lu", (unsigned long)[postData length]];
+    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
+    
+    NSURL *url = [NSURL URLWithString: @"http://54.174.96.55:3000/device/create_session"];
+    
+    [request setURL: url];
+    [request setHTTPMethod:@"POST"];
+    [request setValue:postLength forHTTPHeaderField:@"Content-Length"];
+    [request setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Content-Type"];
+    [request setHTTPBody:postData];
     
     NSURLSession *session = [NSURLSession sharedSession];
-    [[session dataTaskWithURL:[NSURL URLWithString:url]
+    [[session dataTaskWithRequest:request
             completionHandler:^(NSData *data,
                                 NSURLResponse *response,
                                 NSError *error) {
@@ -142,48 +217,11 @@
                 NSString *session_id = [json objectForKey:@"session_id"];
                 [[NSUserDefaults standardUserDefaults] setObject:session_id forKey:@"sessionID"];
                 
+                IntermediateViewController *intermediateVC =
+                [[IntermediateViewController alloc] initWithCount:count];
+                [self presentViewController:intermediateVC animated:YES completion:nil];
+                
             }] resume];
-    
-    IntermediateViewController *intermediateVC =
-        [[IntermediateViewController alloc] initWithCount:count];
-    [self presentViewController:intermediateVC animated:YES completion:nil];
-}
-
-- (void) joinRoom:(NSString *)sessionID {
-    NSString *post = [NSString stringWithFormat:@"phone_id=%@&session_id=%@",
-                      [[NSUserDefaults standardUserDefaults] objectForKey:@"phoneID"],
-                      sessionID];
-    NSData *postData = [post dataUsingEncoding:NSASCIIStringEncoding allowLossyConversion:YES];
-    NSString *postLength = [NSString stringWithFormat:@"%lu", (unsigned long)[postData length]];
-    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
-    
-    [request setURL:[NSURL URLWithString: @"http://54.174.96.55:3000/device/register_session"]];
-    [request setHTTPMethod:@"POST"];
-    [request setValue:postLength forHTTPHeaderField:@"Content-Length"];
-    [request setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Content-Type"];
-    [request setHTTPBody:postData];
-    
-    NSURLSession *session = [NSURLSession sharedSession];
-    [[session dataTaskWithRequest:request
-                completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
-                    if (data) {
-                        NSDictionary *dict =
-                            [NSJSONSerialization JSONObjectWithData:data
-                                                            options:kNilOptions
-                                                              error:&error];
-                        NSLog(@"%@", dict);
-                        int count = [[dict objectForKey:@"count"] intValue];
-                        
-                        [[NSUserDefaults standardUserDefaults] setObject:sessionID forKey:@"sessionID"];
-                        IntermediateViewController *intermediateVC =
-                            [[IntermediateViewController alloc] initWithCount:count];
-                        [self presentViewController:intermediateVC animated:YES completion:nil];
-                        
-                        
-                    } else {
-                       NSLog(@"ERROR");
-                    }
-               }] resume];
 }
 
 
